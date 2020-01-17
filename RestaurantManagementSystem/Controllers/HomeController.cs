428b432093883a1bb47729c2d84f8cc707d89c2c
faruqe25 <faruqe25@gmail.com>
2020-01-18 00:48:41 +0600

    Update

--- RestaurantManagementSystem/Controllers/HomeController.cs
+++ RestaurantManagementSystem/Controllers/HomeController.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using RestaurantManagementSystem.Models;
 
 namespace RestaurantManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        
+         
 
         public IActionResult Index()
         {
-            ViewBag.D = "s f i";
+            
+            
+
             return View();
         }
         
 
         public IActionResult Privacy()
         {
+            
             return View();
         }
 
