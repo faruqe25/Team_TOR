1f312aa2237d120a3a8d57ee155961ae60249b74
Rashedul Islam Sadar <59619110+Sadar1710@users.noreply.github.com>
2020-01-28 00:43:59 +0600

    manager supplier

---
+++ RestaurantManagementSystem/Areas/Manager/Models/CustomerOrderDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Manager.Models
+{
+    public class CustomerOrderDetails
+    {
+        [Key]
+        public int CustomerOrderDetailsID { get; set; }
+        public int FoodItemNo { get; set; }
+        public int Quantity { get; set; }
+        public int CustomerOrderedTableID { get; set; }
+        public int OnlineStatus { get; set; }
+        public int Discount_ID { get; set; }
+    }
+}
---
+++ RestaurantManagementSystem/Areas/Manager/Models/CustomerOrderedTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Manager.Models
+{
+    public class CustomerOrderedTable
+    {
+        [Key]
+        public int CustomerOrderedTableID { get; set; }
+        public int TableID { get; set; }
+        public DateTime BookTimeFrom { get; set; }
+        public DateTime BookTimeTo { get; set; }
+        public DateTime Date { get; set; }
+        public int CustomerID { get; set; }
+        public int ConfirmStatus { get; set; }
+    }
+}
