6ced6b95dbd2c9c157b7cf5567fa3e9cdd4ac21c
Rashedul Islam Sadar <59619110+Sadar1710@users.noreply.github.com>
2020-02-04 11:01:59 +0600

    Update Admin

---
+++ RestaurantManagementSystem/Areas/Admin/Models/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Admin.Models
+{
+    public class FoodItem
+    { [Key]
+        public int FoodItemId { get; set; }
+        public string  FoodName { get; set; }
+        public float  Price { get; set; }
+        public string  Description  { get; set; }
+        public int MealHourId { get; set; }
+    }
+}
---
+++ RestaurantManagementSystem/Areas/Admin/Models/RequiredMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Admin.Models
+{
+    public class RequiredMaterial
+    {
+        [Key]
+        public int RequiredMaterialId { get; set; }
+        public int IngredientId { get; set; }
+        public int FoodItemId { get; set; }
+        public int QuantityInGram { get; set; } 
+
+    }
+}
