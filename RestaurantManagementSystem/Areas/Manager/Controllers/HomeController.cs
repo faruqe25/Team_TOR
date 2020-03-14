21ea53998ad4d76f2177961e0a4869e6f411f853
faruqe25 <faruqe25@gmail.com>
2020-03-14 18:27:27 +0600

    Updation Cart and Table Reservation

---
+++ RestaurantManagementSystem/Areas/Manager/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantManagementSystem.Database;
+
+namespace RestaurantManagementSystem.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Route("Manager/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public HomeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Receipt()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestaurantManagementSystem/wwwroot/js/FoodCartControll.js
@@ -0,0 +1,63 @@
+﻿
+$(document).ready(function () {
+        $("[id^=Quantity]").each(function () {
+            var C = $(this).closest("tr").find("#Quantity").val()
+            if (C == 1) {
+                $(this).closest("tr").find("#Minus").attr("disabled", true);
+            }
+        });
+});
+
+$("table").on("click", "#DeleteMe", function (event) {
+    var FoodId = $(this).closest("tr").find('td:eq(5)').text();
+    var url = "/Home/DeleteCart";
+     $.getJSON(url, {id: FoodId }, function (data) {
+       var FinalCost = data;
+       $("#FinalTotal").html(FinalCost+" tk");
+     });
+    $(this).closest("tr").remove();
+});
+$("button").click(function () {
+     var value = $(this).attr("value");
+    if (value == "+")
+    {
+       var Q = $(this).closest("tr").find("#Quantity").val();
+       var TotalQuantity = Number(Q) + Number(1);
+       $(this).closest("tr").find("#Quantity").val(TotalQuantity);
+        if (TotalQuantity > 1)
+        {
+            $(this).closest("tr").find('#Minus').removeAttr("disabled");
+        }
+       var FoodId = $(this).closest("tr").find('td:eq(5)').text();
+        var url = "/Home/SetCartValueUpdated";
+         $.getJSON(url, {id: FoodId, Quantity: TotalQuantity }, function (data) {
+              var FinalCost = data;
+            $("#FinalTotal").html(FinalCost+" tk");
+         });
+        var FoodPrice = $(this).closest("tr").find("#Ta").val();
+        var CalculatedTotalPrice = Number(FoodPrice) * Number(TotalQuantity)
+        $(this).closest("tr").find("#totalprice").html(CalculatedTotalPrice + " tk");
+
+    }
+     else if (value == "-") {
+         var FoodId = $(this).closest("tr").find('td:eq(5)').text();
+         var FoodPrice = $(this).closest("tr").find("#Ta").val();
+         var Q = $(this).closest("tr").find("#Quantity").val();
+         var TotalQuantity = Number(Q) - Number(1);
+         $(this).closest("tr").find("#Quantity").val(TotalQuantity);
+          $(this).closest("tr").find("#totalprice").html(Number(FoodPrice) * Number(TotalQuantity) + " tk");
+        if (TotalQuantity == 1)
+        {
+            $(this).closest("tr").find("#Minus").attr("disabled", true);
+        }
+
+
+        var url = "/Home/SetCartValueUpdated";
+         $.getJSON(url, {id: FoodId, Quantity: TotalQuantity }, function (data) {
+           var FinalCost = data;
+            $("#FinalTotal").html(FinalCost+" tk");
+
+          });
+     }
+});
+
