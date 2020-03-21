8fc92414a7a6269e8bf59631df6fc5e8273a098b
faruqe25 <faruqe25@gmail.com>
2020-03-21 21:48:41 +0600

    Update Roles And Graph

---
+++ RestaurantManagementSystem/Areas/Manager/ViewModels/TempSell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Manager.ViewModels
+{
+    public class TempSell
+    {
+        public int Day { get; set; }
+        public float Total { get; set; } 
+    }
+}
