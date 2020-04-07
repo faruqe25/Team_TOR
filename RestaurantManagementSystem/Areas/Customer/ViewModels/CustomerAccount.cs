ba24006cc6e0cee046e7aaffcc2f2b34ec3988db
faruqe25 <faruqe25@gmail.com>
2020-04-07 17:02:05 +0600

    Email Validation and minor bug fixes

--- RestaurantManagementSystem/Areas/Customer/ViewModels/CustomerAccount.cs
+++ RestaurantManagementSystem/Areas/Customer/ViewModels/CustomerAccount.cs
@@ -9,14 +9,18 @@ namespace RestaurantManagementSystem.Areas.Customer.ViewModels
     public class CustomerAccount
     {
         [Required]
+        [Display(Name = "Customer Name")]
         public string CustomersName { get; set; }
         [Required]
+        [Display(Name = "Mobile Number")]
         public string MobileNumber { get; set; } 
         [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
-        public string PaymentMobileNumber { get; set; }
+       
 
     }
 }
