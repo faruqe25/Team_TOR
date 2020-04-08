292a48b87d776830ea3f0a46ce60249155d93476
faruqe25 <faruqe25@gmail.com>
2020-04-08 16:07:22 +0600

    Regex Added And Remote Validation is Added

--- RestaurantManagementSystem/Areas/Customer/ViewModels/CustomerAccount.cs
+++ RestaurantManagementSystem/Areas/Customer/ViewModels/CustomerAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,13 +11,18 @@ namespace RestaurantManagementSystem.Areas.Customer.ViewModels
     {
         [Required]
         [Display(Name = "Customer Name")]
+        [RegularExpression(@"([\s]*[A-Za-z]+[\s]*)*", ErrorMessage = "Please provide valid Name")]
+
         public string CustomersName { get; set; }
         [Required]
         [Display(Name = "Mobile Number")]
+        [Remote(action:"IsNumberInUse",controller:"Account")]
+        [RegularExpression(@"[0][1][3-9]{1}[0-9]{8}", ErrorMessage ="Please provide valid Mobile Number")]
         public string MobileNumber { get; set; } 
         [Required]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
+        [Remote(action: "IsEmailInUse", controller: "Account")]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
---
+++ RestaurantManagementSystem/Areas/Customer/ViewModels/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.Areas.Customer.ViewModels
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
